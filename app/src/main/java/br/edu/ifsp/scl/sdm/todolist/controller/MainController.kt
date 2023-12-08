411745c3b1c33d5c3ecadb6dff30a8e2025ff403
Tiago Silas <tiago.rod.oliv@gmail.com>
2023-12-07 23:38:19 -0300

    Adicionado MVC - Funcionando ad e rm item BD

---
+++ app/src/main/java/br/edu/ifsp/scl/sdm/todolist/controller/MainController.kt
@@ -0,0 +1,45 @@
+package br.edu.ifsp.scl.sdm.todolist.controller
+
+import androidx.room.Room
+import br.edu.ifsp.scl.sdm.todolist.model.database.ToDoListDatabase
+import br.edu.ifsp.scl.sdm.todolist.model.database.ToDoListDatabase.Companion.TO_DO_LIST_DATABASE
+import br.edu.ifsp.scl.sdm.todolist.model.entity.Task
+import br.edu.ifsp.scl.sdm.todolist.view.MainFragment
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+//Essas funções são chamadas no main fragment para alteração dos valores no BD
+class MainController(private  val mainFragment: MainFragment) {
+    private val taskDaoImpl = Room.databaseBuilder(
+        mainFragment.requireContext(),
+        ToDoListDatabase::class.java,
+        TO_DO_LIST_DATABASE
+    ).build().getTaskDao()
+
+    fun insertTask(task: Task){
+        CoroutineScope(Dispatchers.IO).launch {
+            taskDaoImpl.createTask(task)
+        }
+    }
+
+    fun getTasks(){
+        CoroutineScope(Dispatchers.IO).launch {
+            var tasks = taskDaoImpl.retrieveTasks()
+            mainFragment.updateTaskList(tasks)
+        }
+    }
+
+    fun editTask(task: Task){
+        CoroutineScope(Dispatchers.IO).launch {
+            taskDaoImpl.updateTask(task)
+        }
+    }
+
+    fun removeTasks(task: Task){
+        CoroutineScope(Dispatchers.IO).launch {
+            taskDaoImpl.deleteTask(task)
+        }
+    }
+
+
+}
